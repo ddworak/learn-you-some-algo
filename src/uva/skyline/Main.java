3cc2ba31a9900ce8023ef1d945c4a015d5a146c3
ddworak <dawid.dworak@gmail.com>
2014-11-21 14:24:52 +0100

    skyline AC

--- src/uva/skyline/Main.java
+++ src/uva/skyline/Main.java
@@ -1,6 +1,6 @@
 package uva.skyline;
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.*;
@@ -12,61 +12,91 @@ import java.util.*;
  * Time: 21:49
  */
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Reader.init(System.in);
-        int n = Reader.nextInt();
         PriorityQueue<Event> events = new PriorityQueue<Event>();
-        for(int i=0;i<n;i++){
-            int s=Reader.nextInt();
-            int e=Reader.nextInt();
-            int h=Reader.nextInt();
-            events.add(new Event(false,s,h));
-            events.add(new Event(true,e,h));
+        int s = Reader.nextInt();
+        while (s != -1) {
+            int h = Reader.nextInt();
+            int e = Reader.nextInt();
+            events.add(new Event(false, s, h));
+            events.add(new Event(true, e, h));
+            s = Reader.nextInt();
         }
         Comparator<Integer> comparator = new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
-                return -Integer.compare(o1,o2);
+                return -Integer.compare(o1, o2);
             }
         };
-        SortedSet<Integer> current = new TreeSet<Integer>(comparator);
-        int last = 0;
-        List<Building> results = new LinkedList<Building>();
-        while(!events.isEmpty()){
+        SortedMap<Integer,Integer> current = new TreeMap<Integer,Integer>(comparator);
+        current.put(0,1);
+        int lasth = -1;
+        int mostAdded=0;
+        boolean lastEnded = true;
+        List<Map.Entry<Integer,Integer>> results = new LinkedList<Map.Entry<Integer, Integer>>();
+        while (!events.isEmpty()) {
             Event c = events.poll();
             //System.out.println("Processing:" + c.p);
-            if(!c.reason){ //starts
-                if(!current.isEmpty() && c.h > current.first()){ //higher than mine, damn
-                    results.add(new Building(last,c.p,current.first()));
-                    last = c.p;
+            if (c.reason) {  //ending
+                if(current.containsKey(c.h) && current.get(c.h)>1){
+                    current.put(c.h,current.get(c.h)-1);
                 }
-                else if(current.isEmpty())last=c.p;
-                //System.out.println("Added:" + c.h);
-                current.add(c.h); //always add to the queue - you never know
+                else current.remove(c.h);
+                //System.out.println("Ending:" + c.h + " current.first=" + current.firstKey() + " c.h=" + c.h + " mostAdded=" + mostAdded);
+                if(current.firstKey()!=c.h && c.h!=mostAdded){
+                    lastEnded=true;
+                    //System.out.println("Last ended:" + c.p);
+                }
             }
-            else {                //ends
-                if(c.h == current.first()){ //it was my highest
-                    results.add(new Building(last,c.p,current.first()));
-                    if(!current.isEmpty())last=c.p;
+            else {                   //starting
+                if(c.h>mostAdded)mostAdded=c.h;
+                //lastEnded=false;
+                //System.out.println("current on q:"+current.size());
+                if(current.containsKey(c.h)){
+                    current.put(c.h,current.get(c.h)+1);
                 }
-                //System.out.println("Removed:" + c.h);
-                current.remove(c.h);
+                else current.put(c.h, 1);
+                //System.out.println("Starting:" + c.h + ",mostAdded=" + mostAdded + " current on q:" + current.size());
+            }
+            if(events.isEmpty() || events.peek().p!=c.p){ //I should decide now!
+                //System.out.println("Made decision");
+                //int temp = results.size();
+                if(lastEnded){
+                    if(mostAdded>current.firstKey() && mostAdded > lasth){
+                        results.add(new AbstractMap.SimpleEntry<Integer, Integer>(c.p,mostAdded));
+                        //if(results.size()>temp)System.out.println("Decided1:"+results.get(results.size()-1));
+                        //mostAdded=0;
+                    }
+                    if(current.firstKey()!=lasth){
+                        //System.out.println("Case "+c.p+ " " + current.first() + " " +lasth);
+                        results.add(new AbstractMap.SimpleEntry<Integer, Integer>(c.p, current.firstKey()));
+                        lasth=current.firstKey();
+                        lastEnded=false;
+                       //if(results.size()>temp)System.out.println("Decided2:"+results.get(results.size()-1)+" currents "+current.size());
+                    }
+                }
+                else if(current.firstKey() > lasth){
+                    results.add(new AbstractMap.SimpleEntry<Integer, Integer>(c.p,current.firstKey()));
+                    //if(results.size()>temp)System.out.println("Decided3:"+results.get(results.size()-1));
+                    lasth=current.firstKey();
+                    lastEnded=false;
+                }
+                mostAdded=0;
+                //System.out.println("");
             }
         }
-        for(Building b : results)System.out.println(b);
-    }
-}
 
-class Building {
-    int s,e,h;
-    @Override
-    public String toString() {
-        return s +" " + e +" " + h;
-    }
-    public Building(int s, int e, int h) {
-        this.s = s;
-        this.e = e;
-        this.h = h;
+        Iterator<Map.Entry<Integer,Integer>> i = results.iterator();
+        if(i.hasNext()){
+            Map.Entry<Integer,Integer> e =i.next();
+            System.out.print(e.getKey()+" "+e.getValue());
+        }
+        while(i.hasNext()){
+            Map.Entry<Integer,Integer> e =i.next();
+            System.out.print(" " + e.getKey()+" "+e.getValue());
+        }
+        System.out.print("\n");
     }
 }
 class Event implements Comparable<Event>{
@@ -83,7 +113,6 @@ class Event implements Comparable<Event>{
         return Integer.compare(p,o.p);
     }
 }
-
 class Reader {
     static BufferedReader reader;
     static StringTokenizer tokenizer;
@@ -94,19 +123,19 @@ class Reader {
         tokenizer = new StringTokenizer("");
     }
     /** get next word */
-    static String next()  {
+    static String next() throws Exception {
         while (!tokenizer.hasMoreTokens()) {
             //TODO add check for eof if necessary
-            try {
-                tokenizer = new StringTokenizer(
-                        reader.readLine());
-            } catch (IOException e) {
-                return "";
-            }
+            tokenizer = new StringTokenizer(
+                    reader.readLine());
         }
         return tokenizer.nextToken();
     }
     static int nextInt() {
-        return Integer.parseInt(next());
+        try {
+            return Integer.parseInt(next());
+        } catch (Exception e) {
+            return -1;
+        }
     }
 }
