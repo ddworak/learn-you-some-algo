ca2761a7949fb3faac5b6760d9218f493bd4c93b
ddworak <dawid.dworak@gmail.com>
2015-04-10 19:13:36 +0200

    removed unnecessary variable in bit op integer addition

--- src/BitFiddle.java
+++ src/BitFiddle.java
@@ -3,17 +3,16 @@
  */
 public class BitFiddle {
     public static int add(int a, int b) {
-        int inter = a;
         int temp;
         while (b != 0) {
-            temp = inter;
-            inter = inter ^ b; //simple sum (no carry)
+            temp = a;
+            a = a ^ b; //simple sum (no carry)
             b = (temp & b) << 1; //carry
         }
-        return inter;
+        return a;
     }
 
     public static void main(String[] args) {
-        System.out.println(add(8, 2));
+        System.out.println(add(8, -2));
     }
 }
